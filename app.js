080dd3d6bdec159d11a1f92b58e50524e2ab87dd
pri65 <priyankasoniii65@gmail.com>
2023-01-07 21:21:21 +0530

    new

--- app.js
+++ app.js
@@ -2,29 +2,50 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-var item=""
+var items= ["Buy Food","Cook Food","Eat Food"];
+var workItems = [];
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req,res) {
     
-    var today = new Date();
+    let today = new Date();
     
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US",options);
+    let day = today.toLocaleDateString("en-US",options);
 
-    res.render("list", {kindOfDay: day, newListItem: item});
+    res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req,res) {
-item = req.body.newItem;
-res.redirect("/");
+let item = req.body.newItem;
+if(req.body.list === "work") {
+workItems.push(item);
+res.redirect("/work");
+} else {
+    items.push(item);
+    res.redirect("/");
+}
 });
+
+
+//new work route
+app.get("/work", function(req,res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work",function(req,res) {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000");
